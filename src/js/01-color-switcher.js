4ac99c5703854850d21abcfaf258942dc85d444c
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2023-10-29 17:42:59 +0100

    17-42--2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,17 @@
+let intervalId = null;
+
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+document.querySelector('[data-start]').addEventListener('click', () => {
+  document.querySelector('[data-start]').disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+document.querySelector('[data-stop]').addEventListener('click', () => {
+  document.querySelector('[data-start]').disabled = false;
+  clearInterval(intervalId);
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,60 @@
+import Notiflix from 'notiflix';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] <= new Date()) {
+          Notiflix.Notify.failure('Please choose a date in the future');
+          document.querySelector('button[data-start]').disabled = true;
+        } else {
+          document.querySelector('button[data-start]').disabled = false;
+        }
+      },
+  };
+
+  const datetimePicker = flatpickr("#datetime-picker", options);
+  let countdownInterval;
+
+  function startTimer() {
+    const targetDate = new Date(datetimePicker.selectedDates[0]);
+    const timer = document.querySelector(".timer");
+    countdownInterval = setInterval(() => {
+      const now = new Date();
+      const timeDifference = targetDate - now;
+      if (timeDifference <= 0) {
+        clearInterval(countdownInterval);
+        timer.innerHTML = "00:00:00:00";
+      } else {
+        const time = convertMs(timeDifference);
+        timer.innerHTML = `${formatTime(time.days)}:${formatTime(time.hours)}:${formatTime(time.minutes)}:${formatTime(time.seconds)}`;
+      }
+    }, 1000);
+  }
+
+  function formatTime(value) {
+    return value.toString().padStart(2, '0');
+  }
+
+  function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  }
+  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+  document.querySelector('button[data-start]').addEventListener('click', startTimer);
