84f509aa84a4cb25de83953f33091ebc557e28e5
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2023-11-01 09:55:24 +0100

    9-55

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,50 @@
+// let intervalId = null;
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+// }
+
+// document.querySelector('[data-start]').addEventListener('click', () => {
+//   document.querySelector('[data-start]').disabled = true;
+//   intervalId = setInterval(() => {
+//     document.body.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+// });
+
+// document.querySelector('[data-stop]').addEventListener('click', () => {
+//   document.querySelector('[data-start]').disabled = false;
+//   clearInterval(intervalId);
+// });
+
 let intervalId = null;
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
 
-document.querySelector('[data-start]').addEventListener('click', () => {
-  document.querySelector('[data-start]').disabled = true;
+function disableStartButton() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+}
+
+function enableStartButton() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+startButton.addEventListener('click', () => {
+  disableStartButton();
   intervalId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 });
 
-document.querySelector('[data-stop]').addEventListener('click', () => {
-  document.querySelector('[data-start]').disabled = false;
+stopButton.addEventListener('click', () => {
   clearInterval(intervalId);
+  enableStartButton();
 });
+
+
+enableStartButton();
