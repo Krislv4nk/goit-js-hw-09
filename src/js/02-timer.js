56313bd564396a231d94eaa4b5699e83b495757c
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2023-11-01 10:15:15 +0100

    10-15

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,20 +7,57 @@ const options = {
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
+    // onClose(selectedDates) {
+    //     if (selectedDates[0] <= new Date()) {
+    //       Notiflix.Notify.failure('Please choose a date in the future');
+    //       document.querySelector('button[data-start]').disabled = true;
+    //     } else {
+    //       document.querySelector('button[data-start]').disabled = false;
+    //     }
+    //   },
+
     onClose(selectedDates) {
-        if (selectedDates[0] <= new Date()) {
-          Notiflix.Notify.failure('Please choose a date in the future');
-          document.querySelector('button[data-start]').disabled = true;
-        } else {
-          document.querySelector('button[data-start]').disabled = false;
-        }
-      },
+      const startButton = document.querySelector('button[data-start]');
+      const datetimeInput = document.querySelector("#datetime-picker");
+      
+      if (selectedDates[0] <= new Date()) {
+        Notiflix.Notify.failure('Please choose a date in the future');
+        startButton.disabled = true;
+        datetimeInput.disabled = false;
+      } else {
+        Notiflix.Notify.success('Date selected. You can start the timer now.');
+        startButton.disabled = false;
+        datetimeInput.disabled = true;
+      }
+    },
   };
 
   const datetimePicker = flatpickr("#datetime-picker", options);
   let countdownInterval;
 
+  // function startTimer() {
+  //   const targetDate = new Date(datetimePicker.selectedDates[0]);
+  //   const timer = document.querySelector(".timer");
+  //   countdownInterval = setInterval(() => {
+  //     const now = new Date();
+  //     const timeDifference = targetDate - now;
+  //     if (timeDifference <= 0) {
+  //       clearInterval(countdownInterval);
+  //       timer.innerHTML = "00:00:00:00";
+  //     } else {
+  //       const time = convertMs(timeDifference);
+  //       timer.innerHTML = `${formatTime(time.days)}:${formatTime(time.hours)}:${formatTime(time.minutes)}:${formatTime(time.seconds)}`;
+  //     }
+  //   }, 1000);
+  // }
+
+
   function startTimer() {
+    const startButton = document.querySelector('button[data-start]');
+    const datetimeInput = document.querySelector("#datetime-picker");
+    startButton.disabled = true;
+    datetimeInput.disabled = false;
+    
     const targetDate = new Date(datetimePicker.selectedDates[0]);
     const timer = document.querySelector(".timer");
     countdownInterval = setInterval(() => {
@@ -29,13 +66,14 @@ const options = {
       if (timeDifference <= 0) {
         clearInterval(countdownInterval);
         timer.innerHTML = "00:00:00:00";
+        startButton.disabled = false;
+        datetimeInput.disabled = true;
       } else {
         const time = convertMs(timeDifference);
         timer.innerHTML = `${formatTime(time.days)}:${formatTime(time.hours)}:${formatTime(time.minutes)}:${formatTime(time.seconds)}`;
       }
     }, 1000);
   }
-
   function formatTime(value) {
     return value.toString().padStart(2, '0');
   }
