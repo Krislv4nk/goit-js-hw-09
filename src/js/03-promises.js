c6a618e1850d379177fa772a2ca681155556d442
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2023-11-01 11:35:14 +0100

    11-35

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,24 +10,66 @@ import Notiflix from 'notiflix';
 //     // Reject
 //   }
 // }
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
 
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
-}
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`Fulfilled promise ${position}`, `Resolved in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`Rejected promise ${position}`, `Rejected in ${delay}ms`);
-  });
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     Notiflix.Notify.success(`Fulfilled promise ${position}`, `Resolved in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notiflix.Notify.failure(`Rejected promise ${position}`, `Rejected in ${delay}ms`);
+//   });
+
+
+
+
+// це треш) але дуже цікаво !!!
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  // console.log("Form submitted");
+  const amount = parseInt(document.getElementById("amount").value);
+const delay = parseFloat(document.getElementById("delay").value);
+const step = parseFloat(document.getElementById("step").value); 
+
+  function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, delay + i * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position}, Resolved in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position}, Rejected in ${delay}ms`);
+      });
+  }
+});
+
+
+
+
+
+
